c5caa20248c8690e812c067262ec90dc904684ea
premsurana-hbwsl <prem.surana@hbwsl.com>
2021-07-01 16:32:49 +0530

    Code Splitting webpack for react routers

--- reactformbuilder.php
+++ reactformbuilder.php
@@ -63,9 +63,6 @@ function html() {
 			<meta name="viewport" content="width=device-width"/>
 			<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 			<title><?php esc_html_e( 'WPLegalPages &rsaquo; Wizard', 'wplegalpages' ); ?></title>
-			<?php do_action( 'admin_print_styles' ); ?>
-			<?php do_action( 'admin_print_scripts' ); ?>
-			<?php do_action( 'admin_head' ); ?>
 		</head>
 		<body class="wplegal-wizard">
 			<div id="root"></div>
--- src/index.js
+++ src/index.js
@@ -1,19 +1,74 @@
 import ReactDOM from "react-dom";
-import BaseComponent from "./components/BaseComponent";
 import { ListContextProvider } from "./components/ListContext";
 
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
+import loadable from "loadable-components";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 import './style.scss';
+
+const Loading = () => {
+  return(
+    <h1 style={{fontSize: '200px'}} className="loading">......Loading</h1>
+  )
+}
+
+const BaseComponent = loadable(() => import("./components/BaseComponent"), {
+  fallback: <Loading />
+});
 
 const App = () => {
   return (
-    <ListContextProvider>
-     <DndProvider backend={HTML5Backend}>
-        <BaseComponent />
-      </DndProvider>
-    </ListContextProvider>
+
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+          <ListContextProvider>
+            <DndProvider backend={HTML5Backend}>
+                <BaseComponent />
+              </DndProvider>
+            </ListContextProvider>
+          </Route>
+          <Route path="/about">
+            <div className="About">About</div>
+          </Route>
+          <Route path="/dashboard">
+            <div className="dashboard">Dashboard</div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
